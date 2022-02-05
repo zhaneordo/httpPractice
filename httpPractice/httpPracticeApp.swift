8ba8635b2d9eda8ab95f1bec201634cbfeb8bb1c
Zhaneordo <smraz9118@gmail.com>
2022-02-05 11:36:08 -0800

    Initial Commit

---
+++ httpPractice/httpPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  httpPracticeApp.swift
+//  httpPractice
+//
+//  Created by Zhaneordo on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct httpPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
